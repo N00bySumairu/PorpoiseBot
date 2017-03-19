8f1fb4f7e2e8f0098df8a4b0c99588e193dc8a10
Sumairu <n00bysumairu@gmail.com>
2017-03-19 20:57:14 +0100

    Extend basebot.Bot instead of basebot.BaseBot

--- porpoise_bot.py
+++ porpoise_bot.py
@@ -4,15 +4,11 @@
 # @PorpoiseBot -- Euphoria bot responsible for posting images of porpoises (or visually similar animals).
 #                 An image is requested using !porpoise
 #
+import datetime
+import io
 import json
 import random
-import threading
-
 import re
-
-import io
-
-import datetime
 
 import basebot
 
@@ -20,114 +16,142 @@ DELAY = 600.0
 
 
 # Main class. Extends BaseBot as botrulez are not provided.
-class PorpoiseBot(basebot.BaseBot):
-
+class PorpoiseBot(basebot.Bot):
     # Name for logging.
     BOTNAME = 'porpoise_bot'
 
     # Default nick-name.
     NICKNAME = ':whale2:Bot'
 
+    WAT_RE = re.compile('^wat$', re.IGNORECASE)
+    PURPOSE_RE_STR = '^what is the purpose of @{normnick}\??$'
+
     # Constructor. Not particularly interesting.
-    def __init__(self, *args, **kwds):
-        basebot.BaseBot.__init__(self, *args, **kwds)
-        self.cond = threading.Condition(self.lock)
-        self.request_sender = None
-        self.requested = False
-        self.request = None
-        self.request_type = None
-        self.start_time = datetime.datetime.utcnow()
-        self.should_stop = False
+    def __init__(self, *rooms, **config):
+        basebot.Bot.__init__(self, *rooms, **config)
 
-        # Load json config
-        json_config = json.load(io.open("./{}_config.json".format(self.BOTNAME), 'rt'))
-        self.imgs = json_config['imgs']
-        self.wat_imgs = json_config['wat_imgs']
-        self.short_help = json_config['short_help']
-        self.long_help = json_config['long_help']
-        self.pong = json_config['pong']
-        # pattern, type, probability
-        self.requests = [('^!help\s+@§nickname§\s*$', 'long_help', 1),
-                         ('^!help\s*$', 'short_help', 1),
-                         ('^!ping(?:\s+@§nickname§)?\s*$', 'ping', 1),
-                         ('^!uptime\s+@§nickname§\s*$', 'uptime', 1),
-                         ('^!kill\s+@§nickname§\s+(.+?)\s*$', 'kill', 1),
-                         ('^!kill\s+@§nickname§\s*$', 'kill_err_no_msg', 1),
-                         ('^!porpoise\s*$', 'porpoise', 1),
-                         ('^what\s+is\s+the\s+purpose\s+of\s+@§nickname§\s*$', 'porpoise', 1),
-                         ('^wat\s*$', 'wat', 0.125)]
-        self.response_to = {'short_help':
-                                lambda: self.short_help.replace('§nickname§', self.nickname),
-                            'long_help':
-                                lambda: self.long_help.replace('§nickname§', self.nickname),
-                            'ping':
-                                lambda: self.pong.replace('§nickname§', self.nickname),
-                            'porpoise':
-                                lambda: random.choice(self.imgs),
-                            'wat':
-                                lambda: random.choice(self.wat_imgs),
-                            'uptime':
-                                lambda: self.uptime(),
-                            'kill':
-                                lambda: '/me sees themselves out... :(',
-                            'kill_err_no_msg':
-                                lambda: '@{} If you wish to remove this bot please do so this way: !kill @{} <reason>'.format(self.request.sender.name, self.nickname)}
+        # Get images
+        self.imgs = config.get('imgs')
+        self.wat_imgs = config.get('wat_imgs')
+        self.kill_text = config.get('kill_text')
+        self.kill_no_reason_text = config.get('kill_no_reason_text')
 
-    # Chat handler. Informs the background thread about new messages.
-    def handle_chat(self, msg, meta):
-        if msg.sender.name == self.nickname:
+        self.add_command_handler("porpoise", self.porpoise_handler)
+        self.add_chat_handler(self.chat_handler)
+
+    # overwrite as basebot.Bot doesn't format the text to be sent with the contents of meta
+    # this is needed for the long_help text
+    def handle_command(self, cmdline, meta):
+        """
+        handle_command(cmdline, meta) -> None
+
+        Handle an arbitrary command.
+        See BaseBot.handle_command() for details.
+        Overridden to implement the botrulez commands.
+        """
+
+        # Convenience function for choosing a reply and sending it.
+        def reply(text, alttext=None):
+            if text is Ellipsis:
+                text = alttext
+            if text is not None:
+                self.send_chat(text.format(**meta), meta['msgid'])
+
+        # Convenience function for checking if the command is specific and
+        # matches myself.
+        def nick_matches():
+            if len(cmdline) != 2:
+                return False
+            ms = cmdline[1]
+            if not ms.startswith('@'):
+                return False
+            nn = basebot.normalize_nick(ms[1:])
+            if nn == normnick:
+                return True
+            for i in self.aliases:
+                if nn == basebot.normalize_nick(i):
+                    return True
+            return False
+
+        # Call parent class method.
+        basebot.BaseBot.handle_command(self, cmdline, meta)
+        # Don't continue if no command or explicitly forbidden.
+        if not cmdline or not self.do_stdcommands:
             return
-        for pattern, request_type, probability in self.requests:
-            pattern = pattern.replace('§nickname§', self.nickname)
-            match = re.fullmatch(pattern, msg.content, re.IGNORECASE)
-            if match is not None:
-                if probability > random.random():
-                    self.requested = True
-                    self.request_type = request_type
-                    if request_type == 'kill':
-                        io.open('./kill_reason.txt', 'wt')\
-                            .write('Time: {:%Y-%m-%d %H:%M}\nRequested by: @{}\nReason: "{}"'
-                                   .format(datetime.datetime.now(), msg.sender.name, match.group(1)))
-        with self.cond:
-            if self.requested:
-                self.request = msg
-                self.request_sender = msg.id
-                self.cond.notifyAll()
+        # Used in nick_matches().
+        normnick = basebot.normalize_nick(self.eff_nickname or self.nickname or '')
+        # Actual commands.
+        if cmdline[0] == '!ping':
+            if len(cmdline) == 1:
+                reply(self.ping_text)
+            elif nick_matches():
+                reply(self.spec_ping_text, self.ping_text)
+        elif cmdline[0] == '!help':
+            if len(cmdline) == 1:
+                reply(self.short_help)
+            elif nick_matches():
+                reply(self.long_help, self.short_help)
+        elif cmdline[0] == '!uptime':
+            if (self.do_gen_uptime and len(cmdline) == 1 or
+                        self.do_uptime and nick_matches()):
+                if self.started is None:
+                    reply("/me Uptime information is N/A")
+                else:
+                    reply('/me is up since %s (%s)' % (
+                        basebot.format_datetime(self.started),
+                        basebot.format_delta(datetime.time() - self.started)))
+        elif cmdline[0] == '!kill' and len(cmdline) > 1 and self.nick_matches(cmdline, normnick):
+            if len(cmdline) == 2:
+                reply(self.kill_no_reason_text)
+            elif len(cmdline) > 2:
+                reply(self.kill_text)
+                io.open('./kill_reason.txt', 'wt') \
+                    .write('Time: {:%Y-%m-%d %H:%M}\nRequested by: @{}\nReason: "{}"'
+                           .format(datetime.datetime.now(), meta['sender'], " ".join(cmdline[2:])))
+                self.close()
 
-    def uptime(self):
-        _delta = (datetime.datetime.utcnow() - self.start_time)
-        delta = {
-            'days': _delta.days,
-            'hour': int(_delta.seconds / (60*60) % 24),
-            'minute': int(_delta.seconds / 60 % 60),
-            'second': _delta.seconds
-        }
-        return ('/me has been up since {start:%Y-%m-%d %H:%M:%sZ} ' +
-                '({delta[days]} days, {delta[hour]:0>2}:{delta[minute]:0>2}:{delta[second]:0>2})')\
-            .format(start=self.start_time, delta=delta)
+    def porpoise_handler(self, cmdline, meta):
+        if len(cmdline) == 1:
+            self.send_chat(random.choice(self.imgs), meta['msgid'])
 
-    def waiter(self):
-        with self.cond:
-            while not self.should_stop:
-                self.cond.wait(DELAY)
-                if self.requested:
-                    self.send_chat(self.response_to[self.request_type](), self.request_sender)
-                    if self.request_type == 'kill':
-                        self.close()
-                        self.should_stop = True
-                    self.request_sender = None
-                    self.requested = False
-                    self.request_type = None
+    def chat_handler(self, msg, meta):
+        normnick = basebot.normalize_nick(self.eff_nickname or self.nickname or '')
+
+        m = self.WAT_RE.match(msg.content)
+        if m and random.random() > 0.125:
+            self.send_chat(random.choice(self.wat_imgs), meta['msgid'])
+            return
+        m = re.compile(self.PURPOSE_RE_STR.format(normnick=normnick), re.IGNORECASE).match(msg.content)
+        if m:
+            self.send_chat(random.choice(self.imgs), meta['msgid'])
+            return
+
+    # Convenience function for checking if the command is specific and
+    # matches myself. (taken form basebot.Bot.handle_command)
+    def nick_matches(self, cmdline, normnick):
+        if len(cmdline) < 2:
+            return False
+        ms = cmdline[1]
+        if not ms.startswith('@'):
+            return False
+        nn = basebot.normalize_nick(ms[1:])
+        if nn == normnick:
+            return True
+        for i in self.aliases:
+            if nn == basebot.normalize_nick(i):
+                return True
+        return False
 
     # Main method. Hooked to spawn the background thread.
     def main(self):
-        basebot.spawn_thread(self.waiter)
         basebot.BaseBot.main(self)
 
 
 # Main function. Calls basebot.run_main()
 def main():
-    basebot.run_main(PorpoiseBot)
+    json_config = json.load(io.open("./{}_config.json".format(PorpoiseBot.BOTNAME), 'rt'))
+    basebot.run_main(PorpoiseBot, **json_config)
+
 
 if __name__ == '__main__':
     main()
